dafe25826e1a8168c9948316a14d2b07af75e387
30jannik06 <mcyolo17@gmail.com>
2022-10-19 16:25:28 +0200

    Create idTicket.ts

---
+++ src/commands/idTicket.ts
@@ -0,0 +1,18 @@
+import {ChatInputCommandInteraction, Embed, EmbedBuilder, SlashCommandBuilder} from "discord.js";
+import {ticketEmbedColor} from "../util/config/config";
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('id')
+        .setDescription('Gives you the id of the Current Guild!'),
+    async execute(interaction: ChatInputCommandInteraction) {
+        const idEmbed = new EmbedBuilder()
+            .setColor(ticketEmbedColor)
+            .setTitle("Server-ID")
+            .setDescription("Server-ID: ``"+interaction.guild.id+"``")
+            .setFooter({
+                text: "Ticket Bot by .jannik#6908"
+            })
+        return interaction.reply({embeds: [idEmbed], ephemeral: true});
+    },
+};
