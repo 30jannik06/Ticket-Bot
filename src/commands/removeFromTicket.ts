ce03694726c3cff1d5c80dea9cd9718854f23d7c
30jannik06 <mcyolo17@gmail.com>
2022-10-19 16:25:29 +0200

    Create removeFromTicket.ts

---
+++ src/commands/removeFromTicket.ts
@@ -0,0 +1,59 @@
+import {
+    ChatInputCommandInteraction,
+    EmbedBuilder, PermissionsBitField,
+    SlashCommandBuilder
+} from "discord.js";
+import {ticketEmbedColor} from "../util/config/config";
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('remove')
+        .setDescription('Remove User from Ticket.')
+        .addUserOption(option =>
+            option
+                .setName('ticket-user')
+                .setDescription("Choose the user you want to remove from the Ticket.")
+                .setRequired(true)
+        )
+        .addChannelOption(option =>
+            option
+                .setName("ticket-channel")
+                .setDescription("Choose the ticket-channel you want to remove a user.")
+                .setRequired(false)
+        ),
+    async execute(interaction: any) {
+        const newTicketUser = interaction.options.getUser("ticket-user")
+        const choosenTicketChannel = interaction.options.getChannel("ticket-channel")
+
+        if (newTicketUser != null) {
+            if (choosenTicketChannel != null) {
+                choosenTicketChannel.permissionOverwrites.edit(newTicketUser.id, {
+                    ViewChannel: false,
+                    AttachFiles: false,
+                    ChangeNickname: false,
+                    EmbedLinks: false,
+                    SendMessages: false,
+                    UseApplicationCommands: false,
+                    ManageChannels: false
+                })
+            } else {
+                interaction.channel.permissionOverwrites.edit(newTicketUser.id, {
+                    ViewChannel: false,
+                    AttachFiles: false,
+                    ChangeNickname: false,
+                    EmbedLinks: false,
+                    SendMessages: false,
+                    UseApplicationCommands: false,
+                    ManageChannels: false
+                })
+            }
+        } else {
+            const needNameEmbed = new EmbedBuilder()
+                .setColor(ticketEmbedColor)
+                .setTitle("Error")
+                .setDescription("You have to enter a new name!")
+            await interaction.reply({embeds: [needNameEmbed], ephemeral: true})
+        }
+    },
+}
+;
