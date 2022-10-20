2897b37179b9f063917800c3b2b5986df60990a9
30jannik06 <mcyolo17@gmail.com>
2022-10-20 10:48:33 +0200

    Update ticketCloseButton.ts

--- src/events/ticketCloseButton.ts
+++ src/events/ticketCloseButton.ts
@@ -1,5 +1,5 @@
-import {error} from "../util/helper/consoleHelper";
-import {ButtonInteraction, Embed, EmbedBuilder} from "discord.js";
+import {err} from "../util/helper/consoleHelper";
+import {ButtonInteraction, Embed, EmbedBuilder, PermissionsBitField} from "discord.js";
 import {ticketEmbedColor} from "../util/config/config";
 
 module.exports = {
@@ -7,22 +7,35 @@ module.exports = {
     async execute(interaction: ButtonInteraction) {
         try {
             if (interaction.customId === "ticketclose") {
-                const ticketCloseEmbed = new EmbedBuilder()
-                    .setColor(ticketEmbedColor)
-                    .setTitle("Ticket Closed")
-                    .setDescription("The ticket will be closed in 30 sec.")
-                    .setFooter({
-                        text: "Ticket-Bot by .jannik#6908",
-                        iconURL: interaction.guild.iconURL({size: 4096, extension: "png"})
-                    })
+                if (interaction.memberPermissions.has(PermissionsBitField.Flags.Administrator)) {
+                    const ticketCloseEmbed = new EmbedBuilder()
+                        .setColor(ticketEmbedColor)
+                        .setTitle("Ticket Closed")
+                        .setDescription("The ticket will be closed in 30 sec.")
+                        .setFooter({
+                            text: "Ticket-Bot by .jannik#6908",
+                            iconURL: interaction.guild.iconURL({size: 4096, extension: "png"})
+                        })
 
-                interaction.reply({embeds: [ticketCloseEmbed]})
-                setTimeout(() => {
-                    interaction.channel.delete();
-                }, 30000)
+                    interaction.reply({embeds: [ticketCloseEmbed]})
+                    setTimeout(() => {
+                        interaction.channel.delete();
+                    }, 30000)
+
+                } else {
+                    const noRightsEmbed = new EmbedBuilder()
+                        .setColor(ticketEmbedColor)
+                        .setTitle("Not Enoght Rights")
+                        .setDescription("You don't have enough rights to close a ticket.")
+                        .setFooter({
+                            text: "Ticket-Bot by .jannik#6908",
+                            iconURL: interaction.guild.iconURL({size: 4096, extension: "png"})
+                        })
+                    interaction.reply({embeds: [noRightsEmbed], ephemeral: true})
+                }
             }
         } catch (e) {
-            error("Ticket Close Button Event: " + e);
+            err("Ticket Close Button Event: " + e);
             await interaction.reply({content: 'There was an error while executing this command!', ephemeral: true});
         }
     },
