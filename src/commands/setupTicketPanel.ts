e18ff0e3d4ca9e45a8e4be1a8c05a1e8afef19c0
30jannik06 <mcyolo17@gmail.com>
2022-10-20 10:48:27 +0200

    Update setupTicketPanel.ts

--- src/commands/setupTicketPanel.ts
+++ src/commands/setupTicketPanel.ts
@@ -1,6 +1,6 @@
 import {
     ActionRow, ActionRowBuilder, ButtonBuilder, ButtonStyle,
-    EmbedBuilder,
+    EmbedBuilder, PermissionsBitField,
     SlashCommandBuilder
 } from "discord.js";
 import {ticketEmbedColor, ticketOpenButtonEmoji} from "../util/config/config";
@@ -9,19 +9,20 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('setup')
         .setDescription('Creates the ticket panel!')
-        .addChannelOption(option=>
-        option
-            .setName("setupchannel")
-            .setDescription("Channel in which the panel will be created")
-            .setRequired(true)
-        ),
+        .addChannelOption(option =>
+            option
+                .setName("setupchannel")
+                .setDescription("Channel in which the panel will be created")
+                .setRequired(true)
+        )
+        .setDefaultMemberPermissions(PermissionsBitField.Flags.Administrator),
     async execute(interaction: any) {
         const setupChannel = interaction.options.getChannel("setupchannel")
 
         const ticketPanelEmbed = new EmbedBuilder()
             .setColor(ticketEmbedColor)
             .setTitle("Ticket System")
-            .setDescription("Click on the button to create ticket!")
+            .setDescription("Click on the button to create a support ticket!")
             .setFooter({
                 text: "Ticket-Bot by .jannik#6908",
                 iconURL: interaction.guild.iconURL({size: 4096, extension: "png"})
@@ -35,8 +36,18 @@ module.exports = {
         )
 
         interaction.guild.channels.fetch(setupChannel.id)
-            .then(ch=>{
-            ch.send({embeds: [ticketPanelEmbed], components: [ticketPanelButton]});
-        })
+            .then(ch => {
+                ch.send({embeds: [ticketPanelEmbed], components: [ticketPanelButton]});
+            })
+
+        const ticketPanelCreatedEmbed = new EmbedBuilder()
+            .setColor(ticketEmbedColor)
+            .setTitle("Setup Done")
+            .setDescription("The ticket panel has been created.")
+            .setFooter({
+                text: "Ticket-Bot by .jannik#6908",
+                iconURL: interaction.guild.iconURL({size: 4096, extension: "png"})
+            })
+        interaction.reply({embeds: [ticketPanelCreatedEmbed], ephemeral: true})
     },
 };
